78be064862510191088e060765a61d09dd1cc6eb
marting1777 <martin-galli@outlook.com>
2018-12-21 17:47:49 -0300

    Search View Update and Setting Pagination

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search'
 import * as searchView from './views/searchView'
-import { elements } from './views/base'
+import { elements, renderLoader, clearLoader } from './views/base'
 
 /** Global state of the APP 
  * - Search Object
@@ -20,11 +20,14 @@ const controlSearch = async () => {
         state.search = new Search(query)
 
         // 3 - Prepare UI for results
-
+        searchView.clearInput()
+        searchView.clearResults()
+        renderLoader(elements.searchRes)
         // 4 - Search for recipes
         await state.search.getResults()
 
         // 5 - Render results on UI
+        clearLoader()
         searchView.renderResults(state.search.result)
     }
 }
